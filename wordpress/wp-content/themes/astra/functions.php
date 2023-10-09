66d5cb767651cf2aa3db1047ec217adc3f88737b
GustavoOliveros <gusa05+1@gmail.com>
2023-10-08 22:06:58 -0300

    Respaldo

--- wordpress/wp-content/themes/astra/functions.php
+++ wordpress/wp-content/themes/astra/functions.php
@@ -180,3 +180,33 @@ require_once ASTRA_THEME_DIR . 'inc/core/markup/class-astra-markup.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-filters.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-hooks.php';
 require_once ASTRA_THEME_DIR . 'inc/core/deprecated/deprecated-functions.php';
+
+add_filter( 'astra_single_post_navigation', 'astra_change_next_prev_text' );
+
+/**
+ * Function to change the Next Post/ Previous post text.
+ *
+ * @param array $args Arguments for next post / previous post links.
+ * @return array
+ */
+function astra_change_next_prev_text( $args ) {
+	$next_post = get_next_post();
+	$prev_post = get_previous_post();
+	$next_text = false;
+	if ( $next_post ) {
+		$next_text = sprintf(
+			'%s <span class="ast-right-arrow">→</span>',
+			$next_post->post_title
+		);
+	}
+	$prev_text = false;
+	if ( $prev_post ) {
+		$prev_text = sprintf(
+			'<span class="ast-left-arrow">←</span> %s',
+			$prev_post->post_title
+		);
+	}
+	$args['next_text'] = $next_text;
+	$args['prev_text'] = $prev_text;
+	return $args;
+}
